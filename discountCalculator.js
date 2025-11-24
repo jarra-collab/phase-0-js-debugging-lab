147a2f5d82b25e30e8471041b4dd56a17a30bfd6
abdiaziz <abdiazizabdullahi40@gmail.com>
2025-11-24 10:34:39 -0800

    lab debugging

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,19 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
+        debugger;
     }
 
     if (quantity >= 10) {
-        totalPrice *= 0.9;
+        totalPrice *= 0.9; // apply 10% discount
+        debugger;
     }
 
     return totalPrice;
 }
-
 
 
 module.exports = calculateDiscountedPrice;
